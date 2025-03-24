7db56b13d3d5aa612d80cd3245049bf9793f8ba2
taewoocode <taewoocode@gmail.com>
2025-03-25 03:41:05 +0900

    request, response 추가

---
+++ src/main/java/com/example/spring_practice_adv/board/request/BoardListRequest.java
@@ -0,0 +1,6 @@
+package com.example.spring_practice_adv.board.request;
+
+public class BoardListRequest {
+	private String keyword;
+	private String category;
+}
---
+++ src/main/java/com/example/spring_practice_adv/board/response/BoardListResponse.java
@@ -0,0 +1,26 @@
+package com.example.spring_practice_adv.board.response;
+
+import java.time.LocalDateTime;
+
+import com.example.spring_practice_adv.board.entity.Board;
+
+import lombok.Setter;
+
+@Setter
+public class BoardListResponse {
+	private Long id;
+	private String title;
+	private String categoryName;
+	private String writerName;
+	private LocalDateTime createdAt;
+
+	public static BoardListResponse fromEntity(Board board) {
+		BoardListResponse response = new BoardListResponse();
+		response.id = board.getId();
+		response.title = board.getTitle();
+		response.categoryName = "profit";
+		response.writerName = "jayce";
+		response.createdAt = board.getCreatedAt();
+		return response;
+	}
+}
