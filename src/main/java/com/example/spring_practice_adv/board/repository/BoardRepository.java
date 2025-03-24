6a59699619416cc01490dcb537b58ef20ec56a7e
taewoocode <taewoocode@gmail.com>
2025-03-25 03:43:52 +0900

    service 추가

--- src/main/java/com/example/spring_practice_adv/board/repository/BoardRepository.java
+++ src/main/java/com/example/spring_practice_adv/board/repository/BoardRepository.java
@@ -1,8 +1,10 @@
 package com.example.spring_practice_adv.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.spring_practice_adv.board.entity.Board;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
---
+++ src/main/java/com/example/spring_practice_adv/board/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.spring_practice_adv.board.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring_practice_adv.board.entity.Board;
+import com.example.spring_practice_adv.board.repository.BoardRepository;
+import com.example.spring_practice_adv.board.response.BoardListResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final BoardRepository boardRepository;
+
+	public List<BoardListResponse> getAllBoards() {
+		List<Board> findBoard = boardRepository.findAll();
+		Stream<Board> stream = findBoard.stream();
+		Stream<BoardListResponse> boardEntity = stream.map(BoardListResponse::fromEntity);
+		return boardEntity.collect(Collectors.toList());
+	}
+}
