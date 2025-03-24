2e7076553d99fd56d0a1026e53ec83e7364b4c33
taewoocode <taewoocode@gmail.com>
2025-03-25 01:56:54 +0900

    hello

---
+++ src/main/java/com/example/spring_practice_adv/SpringPracticeAdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_adv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeAdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_practice_adv/SpringPracticeAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_adv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
